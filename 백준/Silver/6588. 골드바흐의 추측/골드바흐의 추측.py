dfabcb841b5aff77b5350b9cdabcf9e724defae6
엄태성 <62461364+Taeseong-eom@users.noreply.github.com>
2024-09-09 15:01:04 +0900

    [Silver I] Title: 골드바흐의 추측, Time: 580 ms, Memory: 47880 KB -BaekjoonHub

--- 백준/Silver/6588. 골드바흐의 추측/골드바흐의 추측.py
+++ 백준/Silver/6588. 골드바흐의 추측/골드바흐의 추측.py
@@ -4,7 +4,6 @@ import math
 input = sys.stdin.read
 
 def sieve_of_eratosthenes(max_n):
-    # 에라토스테네스의 체를 이용해 max_n까지의 소수를 구함
     is_prime = [True] * (max_n + 1)
     is_prime[0] = is_prime[1] = False  # 0과 1은 소수가 아님
 
@@ -18,25 +17,20 @@ def sieve_of_eratosthenes(max_n):
     return is_prime, odd_primes
 
 def find_goldbach_pair(n, is_prime, odd_primes):
-    # n = a + b 형태의 a, b 찾기
     for a in odd_primes:
-        if a >= n:  # a가 n보다 크면 더 이상 볼 필요 없음
+        if a >= n:  
             break
         b = n - a
-        if is_prime[b]:  # b가 소수이면 조건 만족
+        if is_prime[b]: 
             return a, b
-    return None  # 찾을 수 없는 경우
+    return None
 
-# 입력 처리
 numbers = list(map(int, input().split()))
 
-# 입력 중 가장 큰 수 찾기
 max_number = max(numbers)
 
-# 소수 리스트 생성 (입력의 최대값까지)
 is_prime, odd_primes = sieve_of_eratosthenes(max_number)
 
-# 출력 처리
 for n in numbers:
     if n == 0:
         break
